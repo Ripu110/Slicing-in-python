be0d3b5e4b19456395227f8fce6e47fa84539f5d
RipuDamanSingh <ripu110daman@gmail.com>
2026-05-09 00:08:18 +0000

    this is a commit message

--- slicing.py
+++ slicing.py
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+#slicing in array by constructor.
+>>>>>>> Stashed changes
 import array as arr
 
 A= arr.array("i",[11,12,13,14,15])
@@ -8,3 +12,15 @@ print(A[s1])
 s2= slice(1,4)
 print(A[s2])
 
+<<<<<<< Updated upstream
+=======
+s3 = slice(1,4,2)
+print(A[s3])
+
+
+# Slicing by Notation
+import array as arr
+
+A= arr.array("i",[11,12,13,14,15])
+print(A[:]) #complete arrary
+>>>>>>> Stashed changes
